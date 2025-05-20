ac4a4406703c4e185c790fb94ae661c0262f31d6
Vilembraw <vilembraw@gmail.com>
2025-05-20 16:20:57 +0200

    testy 1c

--- testy/src/test/java/Testy.java
+++ testy/src/test/java/Testy.java
@@ -1,4 +1,5 @@
 import org.example.Playlist;
+import org.example.Song;
 import org.junit.Test;
 
 import java.util.List;
@@ -12,5 +13,13 @@ public class Testy {
         //czy nowo utworzona playlista jest pusta
         Playlist playlist = new Playlist();
         assertEquals(playlist.isEmpty(), true);
+    }
+
+    @Test
+    public void dodanie1(){
+        //Napisz test sprawdzający, czy po dodaniu jednego utworu playlista ma rozmiar 1.
+        Playlist playlist = new Playlist();
+        playlist.add(new Song("Dio","Holydiver", 60));
+        assertEquals(playlist.size(), 1);
     }
 }
